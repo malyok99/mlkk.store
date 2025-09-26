36fe300ec58a1addc6505be813a02e7d6682631a
malyok99 <limeon312@gmail.com>
2025-09-25 22:06:21 -0600

    Initial commit

---
+++ store/src/main/java/com/mlkk/store/HomeController.java
@@ -0,0 +1,12 @@
+package com.mlkk.store;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+public class HomeController {
+    @GetMapping("/")
+    public String index(){
+        return "index.html";
+    }
+}
